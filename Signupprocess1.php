bb1033b7ef687bc51e47f5bcc76978c303d61d3a
31005646 <31005646@my.shu.ac.uk>
2022-03-24 17:54:55 +0000

    master

---
+++ Signupprocess1.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=#, initial-scale=1.0">
+    <title>Please fill in your details</title>
+    <link rel="stylesheet" href="css/mobile.css">
+    <link rel="stylesheet" media="only screen and (min-width:720px)" href="css/desktop.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <link rel="shortcut icon" href="images/logoBlack.png" type="image/x-icon" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<body>
+<div id="pageContainer">
+        <div class="container">
+            <header>
+
+                <div class="logoContainer">
+                <a href="index.php">
+                    <img alt="logo" src="images/logoBlack.png"> 
+                </a>
+                </div>
+        </div>
+    </div> <br>
+    <div id="signUpprocess">
+                    <form action="include">
+                            <label for="username">Username:</label><br>
+                            <input name="username" type="text" class="input"><br>
+                            <label for="password">Password:</label><br>
+                            <input name="password" type="text" class="input"><br>
+                            <label for="email">Email:</label><br>
+                            <input name="email" type="email" class="input"><br>
+                            <label for="secondEmail">Backup Email:</label><br>
+                            <input name="secondEmail" type="email" class="input"><br>
+                            <label for="firstName">First Name:</label><br>
+                            <input name="firstName" type="text" class="input"><br>
+                            <label for="lastName">Last Name:</label><br>
+                            <input name="lastName" type="text" class="input"><br>
+                            <label for="phoneNumber">Phone Number:</label><br>
+                            <input name="phoneNumber" type="text" class="input"><br>
+                            <label for="address">Address:</label><br>
+                            <input name="address" type="text" class="input"> <br>
+                            <label for="messagebox">MessageBox</label>
+                </form>
+    </div>
+</form>
+</body>
+</html>
